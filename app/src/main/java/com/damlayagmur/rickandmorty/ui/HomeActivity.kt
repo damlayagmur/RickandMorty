eec9c0b37ad48b9dc3e478d8480a736ad65c2b47
damlayagmur <dmlygmr@windowslive.com>
2022-01-20 20:41:15 +0300

    add navigation

--- app/src/main/java/com/damlayagmur/rickandmorty/ui/HomeActivity.kt
+++ app/src/main/java/com/damlayagmur/rickandmorty/ui/HomeActivity.kt
@@ -15,8 +15,6 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
