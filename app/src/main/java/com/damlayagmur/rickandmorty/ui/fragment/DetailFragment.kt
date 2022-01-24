095906ccbdbf64c92c080bade2a206f9488e3f62
damlayagmur <dmlygmr@windowslive.com>
2022-01-24 14:36:40 +0300

    add Search menu

--- app/src/main/java/com/damlayagmur/rickandmorty/ui/fragment/DetailFragment.kt
+++ app/src/main/java/com/damlayagmur/rickandmorty/ui/fragment/DetailFragment.kt
@@ -1,4 +1,15 @@
 package com.damlayagmur.rickandmorty.ui.fragment
 
-class DetailFragment {
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.damlayagmur.rickandmorty.R
+import com.damlayagmur.rickandmorty.util.showToast
+
+class DetailFragment: Fragment(R.layout.fragment_detail) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        showToast(requireContext(),"message")
+    }
 }
