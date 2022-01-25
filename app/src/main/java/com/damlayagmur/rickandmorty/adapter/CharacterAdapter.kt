707962440b7da7f8c163c1e66bc53fcc5d7e5863
damlayagmur <dmlygmr@windowslive.com>
2022-01-25 22:48:06 +0300

    add display mode list and grid

--- app/src/main/java/com/damlayagmur/rickandmorty/adapter/CharacterAdapter.kt
+++ app/src/main/java/com/damlayagmur/rickandmorty/adapter/CharacterAdapter.kt
@@ -1,9 +1,14 @@
 package com.damlayagmur.rickandmorty.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.damlayagmur.rickandmorty.R
+import com.damlayagmur.rickandmorty.databinding.GridCharacterBinding
 import com.damlayagmur.rickandmorty.databinding.ListCharacterBinding
 import com.damlayagmur.rickandmorty.model.Result
 import com.damlayagmur.rickandmorty.util.layoutInflaterFactory
@@ -12,30 +17,59 @@ import com.squareup.picasso.Picasso
 class CharacterAdapter(private val characterList: List<Result?>?) :
     RecyclerView.Adapter<CharacterAdapter.CharacterViewHolder>() {
 
-    private lateinit var binding: ListCharacterBinding
+    private val LIST_ITEM = 0
+    private val GRID_ITEM = 1
+
+    private lateinit var mListener: onItemClickListener
+
+    interface onItemClickListener{
+        fun onItemClick(position: Int)
+    }
+
+    fun setOnItemClickListener(listener: onItemClickListener){
+
+        mListener = listener
+    }
+
+    //private lateinit var binding: ListCharacterBinding
+    //private lateinit var binding2: GridCharacterBinding
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
-        binding = ListCharacterBinding.inflate(parent.layoutInflaterFactory(), parent, false)
-        return CharacterViewHolder(binding)
+        val view: View
+        if (viewType == GRID_ITEM) {
+            //binding = ListCharacterBinding.inflate(parent.layoutInflaterFactory(), parent, false)
+            view = LayoutInflater.from(parent.context).inflate(R.layout.list_character, parent, false);
+        } else {
+            //binding = ListCharacterBinding.inflate(parent.layoutInflaterFactory(), parent, false)
+            view = LayoutInflater.from(parent.context).inflate(R.layout.grid_character, parent, false);
+
+        }
+        return CharacterViewHolder(view,mListener)
     }
 
     override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
         val currentCharacter = characterList!![position]
-        Picasso.get().load(currentCharacter?.image).into(holder.characterImage)
+        //Picasso.get().load(currentCharacter?.image).into(holder.characterImage)
         holder.characterName.text = currentCharacter?.name
-        holder.characterStatus.text = currentCharacter?.status
-        holder.characterSpecies.text = currentCharacter?.species
+        //holder.characterStatus.text = currentCharacter?.status
+        //holder.characterSpecies.text = currentCharacter?.species
     }
 
     override fun getItemCount(): Int {
         return characterList!!.size
     }
 
-    inner class CharacterViewHolder(binding: ListCharacterBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-        val characterImage: ImageView = binding.imageViewPhoto
-        val characterName: TextView = binding.textViewName
-        val characterStatus: TextView = binding.textViewStatus
-        val characterSpecies: TextView = binding.textViewSpecies
+    inner class CharacterViewHolder(view: View, listener: onItemClickListener) :
+        RecyclerView.ViewHolder(view) {
+        //val characterImage: ImageView = view.findViewById(R.id.imageView_photo)
+        val characterName: TextView = view.findViewById(R.id.textView_name)
+        //val characterStatus: TextView = binding.textViewStatus
+        //val characterSpecies: TextView = binding.textViewSpecies
+
+        init {
+            view.setOnClickListener{
+                listener.onItemClick(adapterPosition)
+            }
+        }
     }
 }
--- app/src/main/java/com/damlayagmur/rickandmorty/ui/fragment/ListFragment.kt
+++ app/src/main/java/com/damlayagmur/rickandmorty/ui/fragment/ListFragment.kt
@@ -1,49 +1,45 @@
 package com.damlayagmur.rickandmorty.ui.fragment
 
-import android.content.Context
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import android.widget.GridLayout
+import android.widget.GridView
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.damlayagmur.rickandmorty.R
 import com.damlayagmur.rickandmorty.adapter.CharacterAdapter
 import com.damlayagmur.rickandmorty.databinding.FragmentListBinding
+import com.damlayagmur.rickandmorty.model.Character
+import com.damlayagmur.rickandmorty.model.Info
 import com.damlayagmur.rickandmorty.model.Result
+import com.damlayagmur.rickandmorty.model.State
+import com.damlayagmur.rickandmorty.util.showLoading
 import com.damlayagmur.rickandmorty.util.showToast
 import com.damlayagmur.rickandmorty.util.viewBinding
-import androidx.lifecycle.Observer
-import com.damlayagmur.rickandmorty.model.Character
-import com.damlayagmur.rickandmorty.model.State
-import com.damlayagmur.rickandmorty.repository.CharacterRepository
-import com.damlayagmur.rickandmorty.util.showLoading
 import com.damlayagmur.rickandmorty.viewmodel.CharacterViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.coroutines.CoroutineContext
 
 @AndroidEntryPoint
 class ListFragment : Fragment(R.layout.fragment_list) {
 
     private lateinit var recyclerView: RecyclerView
     private var characterList: List<Result?>? = null
-    private lateinit var tempArrayList: ArrayList<Result>
+    private var infoList: List<Info?>? = null
+    private lateinit var tempArrayList: ArrayList<Result?>
     private lateinit var characterAdapter: CharacterAdapter
-    lateinit var imageId: Array<String>
-    lateinit var name: Array<String>
-    lateinit var status: Array<String>
-    lateinit var species: Array<String>
-    var index=0
-    var baseContext: Context? = null
+    private var gridLayoutManager: GridLayoutManager = GridLayoutManager(context, 1)
+    var page = 1
+    //var baseContext: Context? = null
 
     private val binding by viewBinding(FragmentListBinding::bind)
 
@@ -57,14 +53,42 @@ class ListFragment : Fragment(R.layout.fragment_list) {
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.setHasFixedSize(true)
 
-        characterList = arrayListOf<Result>()
-        tempArrayList = arrayListOf<Result>()
-        //getCharacterData()
-        observeLiveData()
-        //characterViewModel.getCharacter()
-        //getAPiData()
 
+        /*recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (!recyclerView.canScrollVertically(1)) {
+                    nextPage()
+                }
+            }
+        })*/
+
+        tempArrayList = arrayListOf<Result?>()
+
+        observeLiveData()
+
+
+        binding.gridView.setOnClickListener {
+            //recyclerView.layoutManager.toString()
+            //if(recyclerView.layoutManager == LinearLayoutManager(requireContext())){
+            recyclerView.layoutManager = GridLayoutManager(requireContext(), 1)
+            //}else{
+            //recyclerView.layoutManager = LinearLayoutManager(requireContext())
+            // }
+        }
     }
+
+    /*private fun nextPage(): Int {
+        if (characterList != null) {
+            var lastCharacterResp = characterList!!.last()
+            if (lastCharacterResp != null) {
+                if (lastCharacterResp.info.next != null) {
+                    page += 1
+                }
+            }
+        }
+        return page
+    }*/
 
     private fun observeLiveData() {
         characterViewModel.getCharacter()
@@ -82,13 +106,46 @@ class ListFragment : Fragment(R.layout.fragment_list) {
                 }
             }
         })
+        /*characterViewModel.getInfo()
+        characterViewModel.infoListLiveData.observe(viewLifecycleOwner, Observer { state ->
+            when (state) {
+                is State.Loading -> showLoading(binding.progressBar, true)
+                is State.Success -> {
+                    infoList = state.data
+                    showToast(requireContext(),"aaaa")
+                }
+                is State.Error -> {
+                    showToast(requireContext(), state.message)
+                }
+            }
+        })*/
     }
 
-    private fun setCharacterRecyclerView(characterList1: List<Result?>?){
-        if(characterList?.isNotEmpty() == true){
+    private fun setCharacterRecyclerView(characterList1: List<Result?>?) {
+        if (characterList?.isNotEmpty() == true) {
             characterAdapter = CharacterAdapter(characterList)
             recyclerView.adapter = characterAdapter
 
+            characterAdapter.setOnItemClickListener(object : CharacterAdapter.onItemClickListener {
+                override fun onItemClick(position: Int) {
+                    val action = characterList!![position]?.let {
+                        //val characterId = it.getCharacterId()
+
+                        ListFragmentDirections.actionListFragmentToDetailFragment(
+                            it.name,
+                            it.status,
+                            it.image,
+                            it.episode.size,
+                            it.species,
+                            it.gender,
+                            it.origin.name,
+                            it.location.name
+                        )
+                    }
+                    findNavController().navigate(action!!)
+
+                }
+            })
         }
     }
 
@@ -100,48 +157,63 @@ class ListFragment : Fragment(R.layout.fragment_list) {
 
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
-                TODO("Not yet implemented")
+                return false
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
-
                 tempArrayList.clear()
                 val searchText = newText!!.lowercase(Locale.getDefault())
                 if (searchText.isNotEmpty()) {
-                    while(index<characterList!!.size){
-
-                    }
-                    /*characterList.forEach {
-                        if (it?.name.toLowerCase(Locale.getDefault()).contains(searchText)) {
-
-                            tempArrayList.add(it?)
+                    characterList!!.forEach {
+                        if (it!!.name.toLowerCase(Locale.getDefault()).contains(searchText)) {
+                            tempArrayList.add(it)
                         }
-                    }*/
+                    }
                     recyclerView.adapter!!.notifyDataSetChanged()
+                    setCharacterRecyclerView(tempArrayList)
                 } else {
                     tempArrayList.clear()
-                    //tempArrayList.addAll(characterList)
+                    tempArrayList.addAll(characterList!!)
                     recyclerView.adapter!!.notifyDataSetChanged()
                 }
                 return false
             }
         })
-
         super.onCreateOptionsMenu(menu, inflater)
     }
 
-    /*private fun getCharacterData() {
-        for (i in imageId.indices) {
-            val news = Result(imageId[i], name[i], status[i], species[i])
-            characterList.add(news)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.item_grid) {
+            switchLayout()
+            switchIcon(item)
         }
-        recyclerView.adapter = CharacterAdapter(tempArrayList)
-
-        tempArrayList.addAll(characterList)
+        return super.onOptionsItemSelected(item)
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        baseContext = context
-    }*/
+    private fun switchLayout() {
+
+        if (gridLayoutManager.spanCount == 1) {
+            gridLayoutManager.spanCount = 2
+            recyclerView.layoutManager = gridLayoutManager
+        } else {
+            gridLayoutManager.spanCount = 1
+            recyclerView.layoutManager = gridLayoutManager
+        }
+        characterAdapter.notifyItemRangeChanged(0, characterAdapter.itemCount)
+    }
+
+    private fun switchIcon(item: MenuItem) {
+        if (gridLayoutManager.spanCount == 1) {
+            item.setIcon(R.drawable.ic_baseline_grid_on_24)
+        } else {
+            item.setIcon(R.drawable.ic_baseline_list_24)
+        }
+    }
+
+/*override fun onAttach(context: Context) {
+    super.onAttach(context)
+    baseContext = context
+}*/
 }
+
+
